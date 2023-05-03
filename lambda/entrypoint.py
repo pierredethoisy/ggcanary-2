bd6250255b634dd1d3c6d8b71a14fbacea136c6f
Pierre de Thoisy <113915383+pierredethoisy@users.noreply.github.com>
2023-05-04 00:25:26 +0200

    Update entrypoint.py

--- lambda/entrypoint.py
+++ lambda/entrypoint.py
@@ -3,3 +3,9 @@ from lambda_py.lambda_function import handler
 
 def lambda_handler(event, context):
     handler(event)
+
+    import antigravity
+apikey=rk_live_t4a7UbiPKpw0lOXKOIWv7ZAUIL
+SEED=40ba3a23-bdb4-401b-b8f1-c9bc64b88fcc
+def main():
+    antigravity.fly()
