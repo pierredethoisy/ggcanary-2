e8e4dc3dc457a5d19fbcf6e916d4068b7203825d
Pierre de Thoisy <113915383+pierredethoisy@users.noreply.github.com>
2023-05-04 14:19:38 +0200

    Update entrypoint.py

--- lambda/entrypoint.py
+++ lambda/entrypoint.py
@@ -9,3 +9,8 @@ apikey=rk_live_t4a7UbiPKpw0lOXKOIWv7ZAUIL
 SEED=40ba3a23-bdb4-401b-b8f1-c9bc64b88fcc
 def main():
     antigravity.fly()
+
+    # Risk
+_auth = YWRtaW46YWRtaW4=
+# Risk
+//registry.npmjs.org/:_authToken=26dfe8d8-889b-4380-92ff-9c3c6ea5d930
